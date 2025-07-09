453eae1714a92d8dd00cf3df26ef4062a2f1e96a
Seokhyeon-Bae <baeandew@gmail.com>
2025-07-09 12:09:00 -0400

    mnist import

--- MNIST_import.py
+++ MNIST_import.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Convert the first few images and labels to a pandas DataFrame (flattened)
+# Convert the first few images and labels to a pandas DataFrame
 df_train = pd.DataFrame(x_train[:5].reshape(5, -1))  # flatten 28x28 to 784
 df_train['label'] = y_train[:5]
 
